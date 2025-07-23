967f508702c49a422d3c4244b37073ddd39d6c50
cmtlyt <cmt01964896@taobao.com>
2025-07-23 09:55:51 +0800

    feat: 补充循环依赖检测

--- src/atomic-functions/classnames.ts
+++ src/atomic-functions/classnames.ts
@@ -2,13 +2,17 @@ import { isArray, isObject } from './verify';
 
 type KeyType = string | number;
 
-type InputType = KeyType | Record<KeyType, any> | InputType[];
+type InputType = KeyType | Record<KeyType, any> | null | undefined | symbol | InputType[];
 
-function objectHandler(obj: Record<KeyType, any>, names: KeyType[]) {
-  Object.entries(obj).forEach(([key, value]) => value && names.push(key));
+function objectHandler(obj: Record<KeyType, any>, names: KeyType[], set: Set<InputType>) {
+  Object.entries(obj).forEach(([key, value]) => {
+    if (!value || set.has(key)) return;
+    set.add(key);
+    names.push(key);
+  });
 }
 
-function arrayHandler(arr: InputType[], names: KeyType[]) {
+function arrayHandler(arr: InputType[], names: KeyType[], set: Set<InputType>) {
   /**
    * 传入 names 为了减少扩展数组引来的开销
    *
@@ -17,13 +21,21 @@ function arrayHandler(arr: InputType[], names: KeyType[]) {
    * 扩展运算符会再次遍历数组导致额外开销
    */
   arr.forEach((item) => {
+    if (!item || set.has(item)) return;
+    set.add(item);
+
+    // 数组处理
     if (isArray(item)) {
-      return arrayHandler(item, names);
-    } else if (isObject(item)) {
-      return objectHandler(item, names);
-    } else if (item) {
-      names.push(String(item));
+      return arrayHandler(item, names, set);
     }
+
+    // 对象处理
+    if (isObject(item)) {
+      return objectHandler(item, names, set);
+    }
+
+    // 普通对象
+    names.push(String(item));
   });
   return names;
 }
@@ -32,7 +44,14 @@ function arrayHandler(arr: InputType[], names: KeyType[]) {
  * 根据输入数据生成 className 字符串
  */
 export function classnames(...input: InputType[]) {
-  return arrayHandler(input, []).join(' ');
+  /**
+   * set 处理循环依赖和重复数据
+   *
+   * 以 classnames 为单位隔离循环依赖, 和资源释放
+   *
+   * 对象不存在循环依赖, 因为不会递归对象, 而是直接判断 value 的布尔值
+   */
+  return arrayHandler(input, [], new Set([input])).join(' ');
 }
 
 export const cn = classnames;
--- tests/atomic-functions/classnames.test.ts
+++ tests/atomic-functions/classnames.test.ts
@@ -20,7 +20,16 @@ MFT(({ classnames, cn }) => {
         ['d', { e: true }, false, ['f', 1, ['', { g: true }, [1, 'true']]]],
         '123',
       ),
-    ).toBe('a b d true c d e f 1 g 1 true 123');
+    ).toBe('a b d true c e f 1 g true 123');
+  });
+
+  test('循环依赖', () => {
+    const obj: Record<string, any> = { a: true, b: false, c: true };
+    obj.d = obj;
+    expect(cn(obj)).toBe('a c d');
+    const arr: any[] = [1, 2, 3];
+    arr.push(arr, obj, arr);
+    expect(cn(arr)).toBe('1 2 3 a c d');
   });
 
   test('边界情况', () => {
@@ -29,21 +38,16 @@ MFT(({ classnames, cn }) => {
     // 没传递任何参数
     expect(cn()).toBe('');
     // 对象为 null
-    // @ts-expect-error test
     expect(cn(null, [])).toBe('');
     // 对象为 null 且没传递 keys
-    // @ts-expect-error test
     expect(cn(null)).toBe('');
     // 对象为 undefined
-    // @ts-expect-error test
     expect(cn(undefined)).toBe('');
     // 对象为 Symbol
-    // @ts-expect-error test
     expect(cn(Symbol('test'))).toMatch(/^Symbol\(test\)$/);
     // 对象为 Symbol.for
-    // @ts-expect-error test
     expect(cn(Symbol.for('test'))).toMatch(/^Symbol\(test\)$/);
-    // 空对象
-    expect(cn({})).toBe('');
+    // 空对象, error 也会被当成对象然后遍历 key 进行处理
+    expect(cn({}, new Error())).toBe('');
   });
 });
