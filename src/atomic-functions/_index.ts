064271b6f5d4b30775a69fce2a98c3515d7bace9
cmtlyt <cmt01964896@taobao.com>
2025-07-30 18:09:00 +0800

    fix: 导出调整

--- src/atomic-functions/_index.ts
+++ src/atomic-functions/_index.ts
@@ -1,6 +1,6 @@
 export * from './cache-fn';
 export * from './classnames';
-export * from './complete-props';
+export * from './compute-props';
 export * from './curry';
 export * from './data-storage';
 export * from './pick';
