a807b0d6c60543d5f4bde593db13befa0bca4c20
cmtlyt <cmt01964896@taobao.com>
2025-10-22 15:11:30 +0800

    docs: 补充singleton的jsdoc

--- src/atomic-functions/singleton.ts
+++ src/atomic-functions/singleton.ts
@@ -1,5 +1,19 @@
 import { cacheFn } from './cache-fn';
 
+/**
+ * 单例包装器
+ * @platform web, node, webworker
+ *
+ * @example
+ * ```js
+ * class A {}
+ * const B = singleton(A)
+ * const b = new B()
+ * const b2 = new B()
+ * b === b2 // true
+ * b2 instanceof A // true
+ * ```
+ */
 export function singleton<T extends new (...args: any[]) => any>(ClassName: T): T {
   const proxy = new Proxy(ClassName, {
     construct: cacheFn<Required<ProxyHandler<T>>['construct']>(Reflect.construct),
